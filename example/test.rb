2e87a0a264536572d77300166516ce77c7b3b282
carrot-jung <gwanggeun@weatherpromise.com>
2023-08-10 10:48:54 +0200

    refactor

--- example/test.rb
+++ example/test.rb
@@ -1,16 +1,8 @@
 require 'fileutils'
 require 'aws-sdk-s3'
+require 'tempfile'
 
 puts 'PDF generate start'
-
-def delete_directory(absolute_path)
-  if Dir.exist?(absolute_path)
-    FileUtils.rm_rf(absolute_path)
-    puts "Folder '#{absolute_path}' deleted."
-  else
-    puts "Folder '#{absolute_path}' does not exist."
-  end
-end
 
 def create_folder_if_not_exists(folder_name)
   if Dir.exist?(folder_name)
@@ -21,28 +13,40 @@ def create_folder_if_not_exists(folder_name)
   end
 end
 
-def upload_file_to_s3(bucket_name, file_path, s3_key)
-    s3 = Aws::S3::Resource.new(region: 'eu-west-1')
+def upload_file_to_s3(bucket_name, local_file_path, s3_key)
+  s3 = Aws::S3::Resource.new(region: 'eu-west-1')
   
-    bucket = s3.bucket(bucket_name)
-    obj = bucket.object(s3_key)
-    obj.upload_file(file_path)
-    puts "File '#{file_path}' uploaded to '#{s3_path}'."
+  bucket = s3.bucket(bucket_name)
+  obj = bucket.object(s3_key)
+  obj.upload_file(local_file_path)
+  puts "File '#{local_file_path}' uploaded to '#{s3_key}'."
+end
+
+def generate_pdf(temp_dir)
+  pdf_path = File.join(temp_dir, 'hourly.pdf')
+  system("bundle exec asciidoctor-pdf -a pdf-fontsdir=templates/fonts -a pdf-themesdir=templates/themes -a pdf-theme=customised -o #{pdf_path} templates/hourly.adoc")
+  pdf_path
 end
 
 folder_name = 'tmp'
-# create tmp folder
+# Create tmp folder
 create_folder_if_not_exists(folder_name)
-# absolute_path = File.expand_path(folder_name)
-# puts "PDF generate start : #{absolute_path}"
-# generate PDF 
-system("bundle exec asciidoctor-pdf -a pdf-fontsdir=templates/fonts -a pdf-themesdir=templates/themes -a pdf-theme=customised -o #{folder_name}/hourly.pdf templates/hourly.adoc")
 
-# upload to S3 
-bucket_name = 'bucket-name'
-s3_key = 'destination/hourly.pdf' 
-upload_file_to_s3(bucket_name, "#{folder_name}/hourly.pdf", s3_key)
-s3_path = "s3://#{bucket_name}/#{s3_key}"
+# Use Tempfile to create a temporary directory
+temp_dir = Dir.mktmpdir(nil, folder_name)
 
-# delete folder
-delete_directory(folder_name)
+begin
+  # Generate PDF
+  pdf_path = generate_pdf(temp_dir)
+
+  # Upload to S3
+  bucket_name = 'bucket-name'
+  s3_key = 'destination/hourly.pdf' 
+  upload_file_to_s3(bucket_name, pdf_path, s3_key)
+  s3_path = "s3://#{bucket_name}/#{s3_key}"
+ensure
+  # Clean up: delete the temporary directory
+  FileUtils.remove_entry(temp_dir)
+end
+
+puts "PDF generate complete and uploaded to '#{s3_path}'."
